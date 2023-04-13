9418d11b43e772702bfd7684fb4630c0058eabdf
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-04-13 14:23:17 +0300

    adds app bar v1

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,16 +1,14 @@
+import NavAppBar from 'components/AppBar/AppBar';
 import { Container } from 'components/Container/Container.styled';
 import { Footer } from 'components/Footer/Footer';
-import { AppBar } from 'components/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import MenuAppBar from 'components/AppBar/Appbar2';
 
 const SharedLayout = () => {
   return (
     <Container>
-      <MenuAppBar />
-      <AppBar />
+      <NavAppBar />
       <Outlet />
       <Footer />
       <ToastContainer />
