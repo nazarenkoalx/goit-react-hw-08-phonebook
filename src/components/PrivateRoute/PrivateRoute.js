088e56c0933ffc3b3d9e2562aec72390852db872
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-04-16 14:45:11 +0300

    adds private route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,14 @@
-import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsFetchingCurrentUser } from 'redux/authSlice/selectors';
+import { selectIsLoggedIn } from 'redux/authSlice/selectors';
 
-const PrivateRoute = ({ children, ...routeProps }) => {
-  return <Route {...routeProps}>{children}</Route>;
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+  const isLoggegIn = useSelector(selectIsLoggedIn);
+  const isFetchingCurrentUser = useSelector(selectIsFetchingCurrentUser);
+
+  const shouldRedirect = !isLoggegIn && !isFetchingCurrentUser;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
 export default PrivateRoute;
